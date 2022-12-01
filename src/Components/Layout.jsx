c92864de1b666cbbafaac84ec95daa83253869b2
Hugo-Latreille <hugo.latreille@gmail.com>
2022-12-01 19:22:10 +0100

    :art:design: test custom appBar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,4 +1,5 @@
 import { Layout } from "react-admin";
 import { MyMenu } from "./Menu";
+import MyAppBar from "./AppBar";
 
 export const CustomLayout = (props) => <Layout {...props} menu={MyMenu} />;

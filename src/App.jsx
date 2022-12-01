fc628ff2e304c50d9706c53cb052edfbec9ebc76
Hugo-Latreille <hugo.latreille@gmail.com>
2022-12-01 12:02:55 +0100

    :rocket:feat: start customizing api platform admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,121 @@
-import { HydraAdmin } from "@api-platform/admin";
+import "./App.css";
+import {
+	HydraAdmin,
+	fetchHydra,
+	hydraDataProvider,
+	ResourceGuesser,
+	ListGuesser,
+	FieldGuesser,
+	ShowGuesser,
+	CreateGuesser,
+	InputGuesser,
+	EditGuesser,
+} from "@api-platform/admin";
+import {
+	TextField,
+	ReferenceField,
+	NumberField,
+	Resource,
+	useRecordContext,
+	List,
+	Datagrid,
+	EditButton,
+	ChipField,
+} from "react-admin";
+import { parseHydraDocumentation } from "@api-platform/api-doc-parser";
 
-export default () => <HydraAdmin entrypoint="https://127.0.0.1:8000/api" />;
+function App() {
+	const entrypoint = import.meta.env.VITE_API_ENTRYPOINT;
+
+	const dataProvider = hydraDataProvider({
+		entrypoint,
+		httpClient: fetchHydra,
+		apiDocumentationParser: parseHydraDocumentation,
+		mercure: true,
+		useEmbedded: false,
+	});
+
+	return (
+		<HydraAdmin entrypoint={entrypoint} dataProvider={dataProvider}>
+			<ResourceGuesser name="users" />
+			<ResourceGuesser
+				name="questions"
+				list={QuestionsList}
+				show={QuestionShow}
+				create={QuestionCreate}
+				edit={QuestionEdit}
+			/>
+			<ResourceGuesser name="answers" />
+			<ResourceGuesser name="media" />
+		</HydraAdmin>
+	);
+}
+const QuestionsList = (props) => (
+	<ListGuesser {...props}>
+		<TextField source="id" />
+		<FieldGuesser source="question" />
+		<NumberField source="timer" />
+		<NumberField source="level" />
+		<FieldGuesser source="media" /> */ /*{" "}
+		<ReferenceField label="Answer" source="answer" reference="answers">
+			<TextField source="answer" />
+		</ReferenceField>
+	</ListGuesser>
+);
+
+// const QuestionsList = () => (
+// 	<List>
+// 		<Datagrid>
+// 			<TextField source="id" />
+// 			<TextField source="question" />
+// 			<NumberField source="timer" />
+// 			<NumberField source="level" />
+// 			{/* <TextField label="Answer" source="answer.answer" /> */}
+// 			{/* <TextField label="Answer" source="answer.answer" /> */}
+// 			{/* <FieldGuesser source="media" /> */}
+// 			<ReferenceField label="Answer" source="answer" reference="answer">
+// 				<TextField source="answer" />
+// 			</ReferenceField>{" "}
+// 			*/
+// 		</Datagrid>
+// 	</List>
+// );
+
+const QuestionShow = (props) => (
+	<ShowGuesser {...props}>
+		<TextField source="id" />
+		<FieldGuesser source="question" />
+		<NumberField source="timer" />
+		<NumberField source="level" />
+		<FieldGuesser source="media" /> */ /*{" "}
+		<ReferenceField label="Answer" source="answer" reference="answer">
+			<TextField source="answer" />
+		</ReferenceField>
+	</ShowGuesser>
+);
+
+const QuestionCreate = (props) => (
+	<CreateGuesser {...props}>
+		<InputGuesser source="question" />
+		<InputGuesser source="timer" />
+		<InputGuesser source="level" />
+		{/* <InputGuesser source="media" />  */}
+		{/* <ReferenceField label="Answer" source="answer" reference="answer">
+			<TextField source="answer" />
+		</ReferenceField> */}
+	</CreateGuesser>
+);
+
+const QuestionEdit = (props) => (
+	<EditGuesser {...props}>
+		<InputGuesser source="question" />
+		<InputGuesser source="timer" />
+		<InputGuesser source="level" />
+		{/* <InputGuesser source="media" />  */}
+		{/* <ReferenceField label="Answer" source="answer" reference="answer">
+			<TextField source="answer" />
+		</ReferenceField> */}
+	</EditGuesser>
+);
+
+export default App;

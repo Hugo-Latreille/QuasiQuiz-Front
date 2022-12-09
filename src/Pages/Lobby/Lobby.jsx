77b8bc454755a9db07e0176752131b910047ad81
Hugo-Latreille <hugo.latreille@gmail.com>
2022-12-09 16:18:12 +0100

    :rocket:feat:logique d'affichage utilisateurs dans lobby selon statut

--- src/Pages/Lobby/Lobby.jsx
+++ src/Pages/Lobby/Lobby.jsx
@@ -5,16 +5,18 @@ import { useContext, useEffect } from "react";
 import useAxiosJWT from "../../utils/useAxiosJWT";
 import { gameHasUsersRoute, gamesRoute, usersRoute } from "../../utils/axios";
 import { UserContext } from "../../App";
+import { useState } from "react";
 
 const Lobby = () => {
 	const axiosJWT = useAxiosJWT();
 	const { user } = useContext(UserContext);
+	// const [userPicture, setUserPicture] = useState(null);
+	const [gameId, setGameId] = useState(null);
+	const [otherUsers, setOtherUsers] = useState(null);
 
 	useEffect(() => {
 		let isMounted = true;
 		const controller = new AbortController();
-
-		// - Si une game est ouverte, et qu'il n'en fait pas déjà partie, on ajoute l'utilisateur à cette game
 
 		const isGameOpen = async () => {
 			try {
@@ -24,6 +26,7 @@ const Lobby = () => {
 						headers: { "Authorization": `Bearer ${user.token}` },
 					}
 				);
+				// setUserPicture(userData["hydra:member"][0].avatar);
 				const userId = userData["hydra:member"][0].id;
 
 				const { data } = await axiosJWT.get(`${gamesRoute}?is_open=true`, {
@@ -46,23 +49,23 @@ const Lobby = () => {
 						return console.log("Partie créée");
 					}
 					const thisGame = data["hydra:member"][0];
-					const gameId = data["hydra:member"][0].id;
-					console.log(thisGame.gameHasUsers);
+					setGameId(data["hydra:member"][0].id);
 
+					// - Si une game est ouverte, et qu'il n'en fait pas déjà partie, on ajoute l'utilisateur à cette game
 					// On vérifie si game_has_user ne contient pas déjà l'utilisateur pour la partie en cours
 					const isUserAlreadyGame = thisGame.gameHasUsers
 						.map((gameHasUser) =>
 							gameHasUser.userId.includes(`api/users/${userId}`)
 						)
 						.some((value) => value === true);
-					console.log(isUserAlreadyGame);
+					console.log("isUserAlreadyGame?", isUserAlreadyGame);
 
 					//Si l'utilisateur n'est pas dans game existante, on l'ajoute
 					if (!isUserAlreadyGame) {
 						const { data: addUserInGame } = await axiosJWT.post(
 							gameHasUsersRoute,
 							{
-								game: `/api/games/${gameId}`,
+								game: `/api/games/${data["hydra:member"][0].id}`,
 								userId: `/api/users/${userId}`,
 								isGameMaster: false,
 							}
@@ -82,7 +85,50 @@ const Lobby = () => {
 		};
 	}, []);
 
-	//* 3-si GM, affichage bouton partie et lancement, le game devient fermé, on appelle la route générant les questions, pour les autres joueurs, bouton "rejoindre la partie"
+	//! 4- on récupère tous les utilisateurs de GHU pour cette partie, on stock pseudo/img/MJ, on map pour afficher
+
+	useEffect(() => {
+		let isMounted = true;
+		const controller = new AbortController();
+		const getGameUsers = async () => {
+			try {
+				if (gameId) {
+					const { data: usersInGame } = await axiosJWT.get(
+						`${gameHasUsersRoute}?game=${gameId}`,
+						{
+							signal: controller.signal,
+						}
+					);
+					if (isMounted && usersInGame) {
+						const allGameUsers = usersInGame["hydra:member"];
+
+						const allOtherUsers = allGameUsers
+							// ?.filter((gameUser) => gameUser.userId.email !== user.email)
+							.map((user) => ({
+								id: user.userId.id,
+								pseudo: user.userId.pseudo,
+								avatar: user.userId.avatar,
+								email: user.userId.email,
+								isGameMaster: user.is_game_master,
+							}));
+
+						console.log(allOtherUsers);
+						setOtherUsers(allOtherUsers);
+					}
+				}
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getGameUsers();
+
+		return () => {
+			isMounted = false;
+			controller.abort();
+		};
+	}, [gameId]);
+
+	//! 3-si GM, affichage bouton partie et lancement, le game devient fermé, on appelle la route générant les questions, navigate vers question / pour les autres joueurs, bouton "rejoindre la partie"
 
 	return (
 		<>
@@ -95,33 +141,44 @@ const Lobby = () => {
 					<div className="gm-box">
 						<h4>Vous lui devez allégeance !</h4>
 						<div className="gm-card">
-							<div className="gamer-img"></div>
+							<div className="gamer-img">
+								{otherUsers &&
+									otherUsers
+										?.filter((otherUser) => otherUser.isGameMaster === true)
+										.map((otherUser) => (
+											<img
+												key={otherUser.id}
+												src={`data:image/svg+xml;base64,${otherUser.avatar}`}
+												alt=""
+											/>
+										))}
+							</div>
 							<h3>Pseudo</h3>
 						</div>
 					</div>
 					<div className="gamer-box">
 						<h4>Joueur présents :</h4>
 						<div className="gc-grid">
-							<div className="gamer-card">
-								<div className="gamer-img"></div>
-								<h3>Pseudo</h3>
-							</div>
-							<div className="gamer-card ">
-								<div className="gamer-img user"></div>
-								<h3>Pseudo</h3>
-							</div>
-							<div className="gamer-card">
-								<div className="gamer-img"></div>
-								<h3>Pseudo</h3>
-							</div>
-							<div className="gamer-card">
-								<div className="gamer-img"></div>
-								<h3>Pseudo</h3>
-							</div>
-							<div className="gamer-card">
-								<div className="gamer-img"></div>
-								<h3>Pseudo</h3>
-							</div>
+							{otherUsers &&
+								otherUsers
+									?.filter((otherUser) => otherUser.isGameMaster === false)
+									.map((otherUser) => (
+										<div key={otherUser?.id} className="gamer-card">
+											<div
+												className={
+													otherUser.email === user.email
+														? "gamer-img user"
+														: "gamer-img"
+												}
+											>
+												<img
+													src={`data:image/svg+xml;base64,${otherUser?.avatar}`}
+													alt=""
+												/>
+											</div>
+											<h3>{otherUser?.pseudo}</h3>
+										</div>
+									))}
 						</div>
 						<div className="legend">
 							<ul>

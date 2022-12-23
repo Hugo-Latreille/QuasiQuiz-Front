863649cb36e3d08a47f12945c5f350e3639f39a6
hugo-latreille <hugo.latreille@gmail.com>
2022-12-23 08:21:00 +0100

    test o2switch deployement

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const host = `https://quasiquizback.herokuapp.com`;
+export const host = `https://www.quasiquiz.net`;
 
 export default axios.create({
 	baseURL: host,
